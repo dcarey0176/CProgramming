a9b0b3f82d585f7eaaeb150f145581e578a409a2
dcarey0176 <dcarey0176@floridapoly.edu>
2025-01-16 13:29:47 +0000

    Code first program

---
+++ 01_first_program/first_program.c
@@ -0,0 +1,23 @@
+// This is the first program in C.
+// It prints a welcome message to the screen.
+
+// -- one-line comment
+
+/*
+
+ This is a multi
+ line comment.
+
+*/
+
+#include <stdio.h>
+
+// Create a main function
+int main() {
+    // Call the printf function
+    printf("Welcome\nto\nCOP2271!\n"); // use escape sequence "\<char>" to add a new line. Escape character ("\")
+    printf("I\nwant\nto\ncode!\n");
+    
+
+    return 0; // Exits the function (not needed for main but necessary for other functions)
+}
